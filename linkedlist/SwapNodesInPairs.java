868551971d7aaf8a83613e9a8281d3e14727e1b9
Hemant Rajput <rajput.hemant2001@gmail.com>
2022-02-16 23:35:50 +0530

    created SwapNodesInPairs.java

---
+++ linkedlist/SwapNodesInPairs.java
@@ -0,0 +1,31 @@
+package linkedlist;
+
+public class SwapNodesInPairs {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        ListNode newNode = head;
+        while (newNode != null && newNode.next != null) {
+            int temp = newNode.val;
+            newNode.val = newNode.next.val;
+            newNode.next.val = temp;
+            newNode = newNode.next.next;
+        }
+        return head;
+    }
+}
