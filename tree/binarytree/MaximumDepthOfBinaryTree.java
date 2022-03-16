f47d2bbc88a387a568d19a083dd3930273f23d1b
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-03-16 21:04:25 +0530

    Updated MaximumDepthOfBinaryTree.java
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- tree/binarytree/MaximumDepthOfBinaryTree.java
+++ tree/binarytree/MaximumDepthOfBinaryTree.java
@@ -1,28 +1,9 @@
 package tree.binarytree;
 
-//104. Maximum Depth of Binary Tree
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
 public class MaximumDepthOfBinaryTree {
-    public class TreeNode {
 
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
-
-    public int maxDepth(TreeNode root) {
+    public static int maxDepth(TreeNode root) {
         if (root == null)
             return 0;
         int depth = Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
