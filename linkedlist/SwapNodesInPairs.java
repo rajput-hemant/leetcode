c2982eaf251c6a249078e90cfa215c3e540e9ff9
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-04-06 20:40:44 +0530

    Removed ListNode class
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- linkedlist/SwapNodesInPairs.java
+++ linkedlist/SwapNodesInPairs.java
@@ -2,22 +2,6 @@ package linkedlist;
 
 // 24. Swap Nodes in Pairs
 public class SwapNodesInPairs {
-    public static class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
 
     public ListNode swapPairs(ListNode head) {
         ListNode newNode = head;
