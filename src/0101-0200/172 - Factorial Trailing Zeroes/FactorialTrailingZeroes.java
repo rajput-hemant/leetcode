f899cd004a8bfe89dc1df45e12fe715a14a0348d
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-08-17 14:27:12 +0530

    solved 172. Factorial Trailing Zeroes
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

---
+++ src/0101-0200/172 - Factorial Trailing Zeroes/FactorialTrailingZeroes.java
@@ -0,0 +1,10 @@
+public class FactorialTrailingZeroes {
+  public int trailingZeroes(int n) {
+    int count = 0;
+    while (n > 0) {
+      n /= 5;
+      count += n;
+    }
+    return count;
+  }
+}
