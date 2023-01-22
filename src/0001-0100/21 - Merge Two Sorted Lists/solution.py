ab3b1dba0f9685363998c94ec9a90054f97f50b0
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2023-01-23 01:13:08 +0530

    solved: 21. Merge Two Sorted Lists
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

---
+++ src/0001-0100/21 - Merge Two Sorted Lists/solution.py
@@ -0,0 +1,15 @@
+from definitions.listnode import ListNode
+
+
+class Solution:
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+        if list1 is None:
+            return list2
+        elif list2 is None:
+            return list1
+        elif list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
