987b7c13d74b7c1b65f9c99b6a8694cc529df4b4
rajput-hemant <rajput.hemant2001@gmail.com>
2023-02-13 01:21:32 +0530

    added solution in golang: 26. Remove Duplicates from Sorted Array
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

---
+++ src/0001-0100/026 - Remove Duplicates from Sorted Array/remove_duplicates_from_sorted_array.go
@@ -0,0 +1,13 @@
+package main
+
+func removeDuplicates(nums []int) int {
+	k := 0
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[k] {
+			k++
+			nums[k] = nums[i]
+		}
+	}
+
+	return k + 1
+}
