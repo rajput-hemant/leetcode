a259bee4ea5229bc74348a7dfb12510a796bf028
rajput-hemant <rajput.hemant2001@gmail.com>
2023-02-13 00:39:03 +0530

    solved 83. Remove Duplicates from Sorted List
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

---
+++ src/0001-0100/083 - Remove Duplicates from Sorted List/remove_duplicates_from_sorted_list.go
@@ -0,0 +1,23 @@
+package main
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	current := head
+	for current != nil && current.Next != nil {
+		if current.Val == current.Next.Val {
+			current.Next = current.Next.Next
+		} else {
+			current = current.Next
+		}
+	}
+
+	return head
+}
