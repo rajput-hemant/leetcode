779d046123fadadee8b30af4c0cfc67c72e6c1af
rajput-hemant <rajput.hemant2001@gmail.com>
2023-02-17 12:21:22 +0530

    solved: 783. Minimum Distance Between BST Nodes
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

---
+++ src/0701-0800/783 - Minimum Distance Between BST Nodes/min_distance_btw_bst_nodes.go
@@ -0,0 +1,37 @@
+package main
+
+import "math"
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func minDiffInBST(root *TreeNode) int {
+	var (
+		minDiff = math.MaxInt
+		prev    = -1
+	)
+
+	var dfs func(*TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+
+		dfs(node.Left)
+
+		if prev != -1 {
+			minDiff = int(math.Min(float64(minDiff), float64(node.Val-prev)))
+		}
+		prev = node.Val
+
+		dfs(node.Right)
+	}
+
+	dfs(root)
+
+	return minDiff
+}
