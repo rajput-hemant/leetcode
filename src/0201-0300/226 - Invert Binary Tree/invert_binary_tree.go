af81b5b2061756b34cac2c3eb4d7314a53e2341b
rajput-hemant <rajput.hemant2001@gmail.com>
2023-02-18 11:54:43 +0530

    solved: 226. Invert Binary Tree
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

---
+++ src/0201-0300/226 - Invert Binary Tree/invert_binary_tree.go
@@ -0,0 +1,21 @@
+package main
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	invertTree(root.Left)
+	invertTree(root.Right)
+
+	root.Left, root.Right = root.Right, root.Left
+
+	return root
+}
