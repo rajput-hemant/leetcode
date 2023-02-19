3fa893c8651ca77e3380767009798b0b09c5ff93
rajput-hemant <rajput.hemant2001@gmail.com>
2023-02-20 00:00:29 +0530

    solved: 242. Valid Anagram
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

---
+++ src/0201-0300/242 - Valid Anagram/valid_anagram.rs
@@ -0,0 +1,24 @@
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        if (s.len() != t.len()) {
+            return false;
+        }
+
+        // create vector of chars from the string
+        let mut s_chars = s.chars().collect::<Vec<char>>();
+        let mut t_chars = t.chars().collect::<Vec<char>>();
+
+        // sort the vector of chars
+        s_chars.sort();
+        t_chars.sort();
+
+        // compare the sorted vectors of chars
+        for i in 0..s_chars.len() {
+            if s_chars[i] != t_chars[i] {
+                return false;
+            }
+        }
+
+        true
+    }
+}
