f9ba90fd523231fa3e5125763b607267852675fd
rajput-hemant <rajput.hemant2001@gmail.com>
2023-02-21 11:27:43 +0530

    solved: 540. Single Element in a Sorted Array
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

---
+++ src/0501-0600/540 - Single Element in a Sorted Array/single_element_in_a_sorted_array.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn single_non_duplicate(nums: Vec<i32>) -> i32 {
+        let mut res = 0;
+
+        for num in nums {
+            // a ^ a = 0
+            // a ^ a ^ a = a
+            // a ^ a ^ b = b
+            // also position of a and b doesn't matter
+            res ^= num;
+        }
+
+        res
+    }
+}
