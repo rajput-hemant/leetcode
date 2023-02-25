ab86a5f2395b6b2208bc69ca07bf99f02f55792b
rajput-hemant <rajput.hemant2001@gmail.com>
2023-02-25 20:27:24 +0530

    solved: 350. Intersection of Two Arrays II
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

---
+++ src/0301-0400/350 - Intersection of Two Arrays II/intersection_of_two_arrays_2.rs
@@ -0,0 +1,24 @@
+impl Solution {
+    pub fn intersect(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+        let (mut nums1, mut nums2) = (nums1, nums2);
+        let mut res = Vec::new();
+        let (mut i, mut j) = (0, 0);
+
+        nums1.sort();
+        nums2.sort();
+
+        while i <= nums1.len() - 1 && j <= nums2.len() - 1 {
+            if nums1[i] == nums2[j] {
+                res.push(nums1[i]);
+                i += 1;
+                j += 1;
+            } else if nums1[i] < nums2[j] {
+                i += 1;
+            } else {
+                j += 1;
+            }
+        }
+
+        res
+    }
+}
