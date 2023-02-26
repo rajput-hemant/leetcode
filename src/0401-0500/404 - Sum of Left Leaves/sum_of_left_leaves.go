755bb11dab3a69a298edf202a862a46d8128fea0
rajput-hemant <rajput.hemant2001@gmail.com>
2023-02-26 11:35:46 +0530

    added solution using iteration
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/0401-0500/404 - Sum of Left Leaves/sum_of_left_leaves.go
+++ src/0401-0500/404 - Sum of Left Leaves/sum_of_left_leaves.go
@@ -7,6 +7,7 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// Recursion
 func sumOfLeftLeaves(root *TreeNode) int {
 	if root == nil {
 		return 0
@@ -18,4 +19,35 @@ func sumOfLeftLeaves(root *TreeNode) int {
 	}
 
 	return sumOfLeftLeaves(root.Left) + sumOfLeftLeaves(root.Right)
+}
+
+// Iteration
+func sumOfLeftLeaves2(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	sum := 0
+	stack := []*TreeNode{root}
+
+	for len(stack) > 0 {
+		// pop
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if node.Left != nil {
+			// if node.Left is a leaf node
+			if node.Left.Left == nil && node.Left.Right == nil {
+				sum += node.Left.Val
+			} else {
+				stack = append(stack, node.Left)
+			}
+		}
+
+		if node.Right != nil {
+			stack = append(stack, node.Right)
+		}
+	}
+
+	return sum
 }
