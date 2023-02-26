cda99370f7cae0e0bf95c78773e49f8d939c6dd9
rajput-hemant <rajput.hemant2001@gmail.com>
2023-02-26 10:52:26 +0530

    added solution in rust: 72. Edit Distance
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

---
+++ src/0001-0100/072 - Edit Distance/edit_distance.rs
@@ -0,0 +1,22 @@
+impl Solution {
+    fn min_distance(word1: String, word2: String) -> i32 {
+        let (m, n) = (word1.len(), word2.len());
+        let mut dp = vec![vec![0; n + 1]; m + 1];
+
+        for i in 0..=m {
+            for j in 0..=n {
+                if i == 0 {
+                    dp[i][j] = j as i32;
+                } else if j == 0 {
+                    dp[i][j] = i as i32;
+                } else if word1.chars().nth(i - 1) == word2.chars().nth(j - 1) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + dp[i - 1][j - 1].min(dp[i - 1][j].min(dp[i][j - 1]));
+                }
+            }
+        }
+
+        dp[m][n]
+    }
+}
