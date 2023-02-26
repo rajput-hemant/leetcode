350799a1eba8aa1565eaa75145bb5b42ce08f9ab
rajput-hemant <rajput.hemant2001@gmail.com>
2023-02-26 11:08:19 +0530

    optimized code by using byte slice instead of calling chars() to create new iterator for each iteration of loop
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/0001-0100/072 - Edit Distance/edit_distance.rs
+++ src/0001-0100/072 - Edit Distance/edit_distance.rs
@@ -1,6 +1,7 @@
 impl Solution {
     fn min_distance(word1: String, word2: String) -> i32 {
         let (m, n) = (word1.len(), word2.len());
+        let (word1, word2) = (word1.as_bytes(), word2.as_bytes());
         let mut dp = vec![vec![0; n + 1]; m + 1];
 
         for i in 0..=m {
@@ -9,7 +10,7 @@ impl Solution {
                     dp[i][j] = j as i32;
                 } else if j == 0 {
                     dp[i][j] = i as i32;
-                } else if word1.chars().nth(i - 1) == word2.chars().nth(j - 1) {
+                } else if word1[i - 1] == word2[j - 1] {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
                     dp[i][j] = 1 + dp[i - 1][j - 1].min(dp[i - 1][j].min(dp[i][j - 1]));
