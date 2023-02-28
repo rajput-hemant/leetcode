2970d0316ac29c88e843e81f79191a0989480c1e
rajput-hemant <rajput.hemant2001@gmail.com>
2023-02-28 20:45:38 +0530

    solved: 476. Number Complement
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

---
+++ src/0401-0500/476 - Number Complement/num_complement.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn find_complement(num: i32) -> i32 {
+        let mut res = String::new();
+
+        for ch in format!("{:b}", num).chars() {
+            if ch == '0' {
+                res += "1";
+            } else {
+                res += "0";
+            }
+        }
+
+        i32::from_str_radix(&res, 2).unwrap()
+    }
+}
