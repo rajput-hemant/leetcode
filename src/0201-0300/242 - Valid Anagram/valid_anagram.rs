659a6fdc6b12e7cae2d39337828a9f246aabc529
rajput-hemant <rajput.hemant2001@gmail.com>
2023-07-13 20:03:41 +0530

    add solution in rust using HashMap: 242. Valid Anagram
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/0201-0300/242 - Valid Anagram/valid_anagram.rs
+++ src/0201-0300/242 - Valid Anagram/valid_anagram.rs
@@ -1,4 +1,29 @@
+use std::collections::HashMap;
+
 impl Solution {
+    // using hashmap
+    pub fn is_anagram(s: String, t: String) -> bool {
+        if s.len() != t.len() {
+            return false;
+        }
+
+        let s_map = s.chars().fold(HashMap::new(), |mut map, c| {
+            // *map.entry(c).or_insert(0) = map.get(&c).unwrap_or(&0) + 1;
+            // same as above
+            *map.entry(c).or_insert(0) += 1;
+            map
+        });
+
+        let t_map = t.chars().fold(HashMap::new(), |mut map, c| {
+            *map.entry(c).or_insert(0) += 1;
+            map
+        });
+
+        s_map
+            .iter()
+            .all(|(key, value)| t_map.get(key) == Some(value))
+    }
+
     pub fn is_anagram(s: String, t: String) -> bool {
         if (s.len() != t.len()) {
             return false;
