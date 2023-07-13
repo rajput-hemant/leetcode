bcdb93d6658fdde1fbdbf0fd77e276b1e85e12d0
rajput-hemant <rajput.hemant2001@gmail.com>
2023-07-13 14:55:23 +0530

    add comment blocks for better explanation
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/0401-0500/498 - Diagonal Traverse/diagonal_traverse.rs
+++ src/0401-0500/498 - Diagonal Traverse/diagonal_traverse.rs
@@ -11,41 +11,42 @@ impl Solution {
         let (row_len, col_len) = (mat.len(), mat[0].len());
 
         while row < row_len && col < col_len {
+            // push the current element to the result vector
             result.push(mat[row][col]);
 
             match direction {
                 Direction::Up => {
-                    // if we are at the top right corner, we need to go down
-                    // else we need to go right
                     if row == 0 || col == col_len - 1 {
+                        // if we are at the top row or the rightmost column, change direction to "Down"
                         direction = Direction::Down;
 
-                        // if we are at the top right corner, we need to go down
-                        // else we need to go right
                         if col == col_len - 1 {
+                            // if at the rightmost column, move to the next row
                             row += 1;
                         } else {
+                            // otherwise, move to the next column
                             col += 1;
                         }
                     } else {
+                        // move diagonally upward
                         row -= 1;
                         col += 1;
                     }
                 }
                 Direction::Down => {
-                    // if we are at the bottom left corner, we need to go up
-                    // else we need to go down
                     if col == 0 || row == row_len - 1 {
+                        // if we are at the leftmost column or the bottom row, change direction to "Up"
                         direction = Direction::Up;
 
-                        // if we are at the bottom left corner, we need to go right
-                        // else we need to go down
                         if row == row_len - 1 {
+                            // if at the bottom row, move to the next column
                             col += 1;
                         } else {
+                            // otherwise, move to the next row
                             row += 1;
                         }
                     } else {
+                        // move diagonally downward
                         row += 1;
                         col -= 1;
                     }
